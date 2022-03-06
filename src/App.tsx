a0babb0aac2987e86599a32703571616e3debea9
Valentin Fediuk <valentin.fediuk@gogomedia.pl>
2022-03-06 23:57:02 +0100

    feat: fetching data

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,29 @@
-import React from "react";
-import apiData from "./Api/Api";
+import {useEffect, useState} from "react";
+import apiData from "./api";
 import PersonInfo from "./Components/PersonInfo/PersonInfo";
 
+interface User {
+  "id": string;
+  "jobTitle": string;
+  "emailAddress": string;
+  "firstNameLastName": string;
+}
+
 function App() {
-  const [data, setData] = React.useState([]);
-  const [selected, setSelected] = React.useState([]);
+  const [data, setData] = useState<User[]>([]);
+  const [selected, setSelected] = useState<User[]>([]);
+
+  useEffect(() => {
+    async function FetchUsers() {
+      try {
+        const result = await apiData();
+        setData([...data, ...result]);
+      } catch (error) {
+        console.log(error)
+      }
+    }
+    FetchUsers()
+  }, [])
 
   return (
     <div className="App">
